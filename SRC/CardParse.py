c53006654eab25dde1e2255dd9de31d7f6509e39
Your NameAnthony <anthonyclarence.goo@gmail.com>
2024-07-02 13:58:58 -0700

    Normalize Edhrec Values

--- SRC/CardParse.py
+++ SRC/CardParse.py
@@ -3,8 +3,10 @@ import pandas as pd
 import json
 import os
 import sys
+import numpy as np
 sys.path.append(os.getcwd() + "\\Lib")
 import ParseFunctions as ParseFunc
+import matplotlib.pyplot as plt
 
 f = open('Scryfall Uploads/AllCards_7_1_2024.json', encoding='utf8')
 data = json.load(f)
@@ -13,7 +15,11 @@ print("Data Loaded")
 PandaData = pd.DataFrame.from_records(data)
 Columns = PandaData.columns
 # Columns of Interest
-CoI = ['name','cmc','type_line','oracel_text','color_identity','keywords','legalities','']
+CoI = ['name','cmc', 'mana_cost','type_line','oracel_text','color_identity','keywords','legalities','power','toughness','edhrec_rank']
+
+for x in Columns:
+    if x not in CoI:
+        PandaData = PandaData.drop(x, axis=1)
 
 # I'm a Commander Player, so it's the only format that I care about legality wise
 rowdrop = []
@@ -23,7 +29,19 @@ for index, row in  PandaData.iterrows():
         rowdrop.append(index)                           # create an array with all non-legal card indices
 PandaData = PandaData.drop(rowdrop)                     # Drop all cards that are not commander legal
 print("Dropped ",len(rowdrop), " rows")
-ParseFunc.WriteJsonFromPD(PandaData,'CommanderLegal')
+#ParseFunc.WriteJsonFromPD(PandaData,'CommanderLegal')
 
-print("Create Changes")
+WorstCardValue = max(PandaData[:]['edhrec_rank'])       # I'll be using edhrec rank as a objective measure of a cards approximate value/power
+# Normalize card value
+def EDHRECNormalize(a):
+    if np.isnan(a):
+        a = WorstCardValue
+    Value = a/(WorstCardValue+1e-6)
+    return Value
+PandaData['edhrec_rank'] = PandaData['edhrec_rank'].apply(EDHRECNormalize)
+
+
+
+
+
 print("End")
